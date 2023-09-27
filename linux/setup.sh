41f752fddd340ccf696889ab38d4e6fad1a30702
Davi Reis <dreisss026@gmail.com>
2023-09-27 15:28:56 -0300

    :zap: chore(linux): updated and fixed few errors

--- linux/setup.sh
+++ linux/setup.sh
@@ -13,9 +13,14 @@ sudo adduser aluno
 sudo passwd -d aluno
 
 sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
-sudo apt install virtualbox virtualbox-ext-pack virt-manager vscode python3 vim neovim helix gcc git net-tools -y
+sudo apt install build-essential net-tools -y
 
 sudo wget -O /usr/share/backgrounds/wallpaper.png https://raw.githubusercontent.com/dreisss/iespes-extra/main/design/wallpapers/wallpaper.png
 
 gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/wallpaper.png"
+gsettings set com.zorin.desktop.auto-theme day-theme "ZorinGreen-Light"
+gsettings set com.zorin.desktop.auto-theme night-theme "ZorinGreen-Dark"
+gsettings set org.gnome.desktop.interface icon-theme "ZorinGreen-Dark"
 gsettings set org.gnome.desktop.interface gtk-theme "ZorinGreen-Dark"
+gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+gsettings set org.gnome.shell.extensions.user-theme name "ZorinGreen-Dark"
--- linux/user.sh
+++ linux/user.sh
@@ -10,4 +10,9 @@ then
 fi
 
 gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/wallpaper.png"
+gsettings set com.zorin.desktop.auto-theme day-theme "ZorinGreen-Light"
+gsettings set com.zorin.desktop.auto-theme night-theme "ZorinGreen-Dark"
+gsettings set org.gnome.desktop.interface icon-theme "ZorinGreen-Dark"
 gsettings set org.gnome.desktop.interface gtk-theme "ZorinGreen-Dark"
+gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+gsettings set org.gnome.shell.extensions.user-theme name "ZorinGreen-Dark"
