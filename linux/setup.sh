e0856fcda63d34b6d2f99a6194651aafc561ef73
Davi Reis <dreisss026@gmail.com>
2023-10-05 21:33:34 -0300

    :sparkles: feat(linux): added wifi connection and virtualbox installation

--- linux/setup.sh
+++ linux/setup.sh
@@ -2,12 +2,23 @@
 
 networkInterface="$(nmcli c | awk -F '  ' '{ if (NR==2) print $1 }')"
 
-if [ $(nmcli d | grep "ethernet  conectado" | wc -l) != 1 ]; then
-	nmcli c m "$networkInterface" ipv4.addresses "192.168.$1.$(($2 + 1))/24"
-	nmcli c m "$networkInterface" ipv4.gateway "192.168.$1.1"
-	nmcli c m "$networkInterface" ipv4.dns "8.8.8.8"
-	nmcli c m "$networkInterface" ipv4.method "manual"
-	nmcli c u "$networkInterface"
+if [ $(nmcli d | grep "conectado" -c) -lt 1 ]; then
+	if [ $(nmcli d | grep "wifi" -c) -ge 1 ]; then
+		nmcli d w c "WIFI ALUNOS IESPES"
+	else
+		nmcli c m "$networkInterface" ipv4.addresses "192.168.$1.$(($2 + 1))/24"
+		nmcli c m "$networkInterface" ipv4.gateway "192.168.$1.1"
+		nmcli c m "$networkInterface" ipv4.dns "8.8.8.8"
+		nmcli c m "$networkInterface" ipv4.method "manual"
+		nmcli c u "$networkInterface"
+	fi
+fi
+
+sleep 5
+
+if [ $(nmcli d | grep "conectado" | wc -l) != 1]; then
+	echo "Failed to connect to internet."
+	exit
 fi
 
 if [ $(cat /etc/passwd | grep "aluno" | wc -l) != 1 ]; then
@@ -15,10 +26,12 @@ if [ $(cat /etc/passwd | grep "aluno" | wc -l) != 1 ]; then
 	sudo passwd -d aluno
 fi
 
-sleep 5
-
 sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
 sudo apt install build-essential net-tools cpu-x -y
+
+if [ $1 = 3 ]; then
+	sudo apt install vim virtualbox -y
+fi
 
 sudo wget -O /usr/share/backgrounds/wallpaper.png https://raw.githubusercontent.com/dreisss/iespes-extra/main/design/wallpapers/wallpaper.png
 
