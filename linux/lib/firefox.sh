68bacac5863f1cba511eced85c885a50ebb514ac
Davi Reis <dreisss026@gmail.com>
2023-10-18 18:44:09 -0300

    refactor(linux/firefox): removed unnecessary command

--- linux/lib/firefox.sh
+++ linux/lib/firefox.sh
@@ -2,7 +2,6 @@
 
 if [ ! -f /usr/bin/firefox-default ]; then
 	sudo mv /usr/bin/firefox /usr/bin/firefox-default
-	sudo touch /usr/bin/firefox
 	echo "firefox-default -private-window" | sudo tee /usr/bin/firefox
 	sudo chmod +x /usr/bin/firefox
 fi
