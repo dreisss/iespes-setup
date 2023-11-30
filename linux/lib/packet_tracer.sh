1160d24740cb5876d08aabbe54414804a95e6c77
Davi Reis <dreisss.dev@gmail.com>
2023-11-30 18:47:10 -0300

    fix(linux/cpt): enabled auto confirm

--- linux/lib/packet_tracer.sh
+++ linux/lib/packet_tracer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wget -O ~/Downloads/cpt-8.2.1.deb https://github.com/dreisss/iespes-setup/raw/main/linux/other/cpt-8.2.1.deb
-sudo apt install ~/Downloads/cpt-8.2.1.deb
+sudo apt install ~/Downloads/cpt-8.2.1.deb -y
 rm ~/Downloads/cpt-8.2.1.deb
